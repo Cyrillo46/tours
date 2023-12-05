d356e61e4301a313503945dfdab04287c75d7ce4
Cyrillo46 <yahooisbetter46@gmail.com>
2023-12-05 13:53:55 -0500

    updated classes for App component

--- Tours/src/App.jsx
+++ Tours/src/App.jsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Loading } from "./Loading";
-import { Tours } from "./Tours";
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
   const removeTour = (id) => {
@@ -15,32 +14,45 @@ function App() {
   };
 
   const fetchTours = async () => {
-    setIsLoading(true);
-    const response = await fetch(url);
-    const tour = await response.json();
-    setTours(tour);
-    console.log(response);
-    setIsLoading(false);
+    // will use later
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
-
   useEffect(() => {
     fetchTours();
   }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (tours.length === 0) {
+  if (loading) {
     return (
       <main>
-        <h2>No Tours Remaining</h2>
-        <button onClick={fetchTours}>Refresh Tours</button>
+        <Loading />
       </main>
     );
   }
-
-  return <Tours tours={tours} removeTour={removeTour} />;
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
 export default App;
